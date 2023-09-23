e36a0388f6d24d4aad3458007201eedf6d934524
81GB3N <jonas.balukonis@gmail.com>
2023-09-23 17:33:47 +0300

    got checker data

--- templates/main.js
+++ templates/main.js
@@ -1,4 +1,8 @@
-const square = document.querySelectorAll(".square");
-square.forEach(() => {
-    const 
+document.addEventListener('mousedown', (event) => {
+    const elementUnderMouse = document.elementFromPoint(event.clientX, event.clientY);
+
+    if (elementUnderMouse.className.includes("active")) {
+        const currentChecker = elementUnderMouse.dataset.checker;
+
+    }
 });
