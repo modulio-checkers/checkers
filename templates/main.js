e4cec72082b10e976a0d4ff902315197a55bc593
81GB3N <jonas.balukonis@gmail.com>
2023-09-24 11:17:27 +0300

    nesiseka biski

--- templates/main.js
+++ templates/main.js
@@ -1,5 +1,7 @@
 const checkerMove = document.querySelector(".player");
-let isDragging;
+let isDragging = false;
+const pActive = document.querySelector(".pActive");
+
 
 document.addEventListener('mousedown', (event) => {
     isDragging = false;
@@ -7,6 +9,7 @@ document.addEventListener('mousedown', (event) => {
         const currentChecker = elementUnderMouse.dataset.checker;
         if(elementUnderMouse.className.includes("active")){
             isDragging = true;
+            checkerMove.classList.add("pActive");
         }
         // const pActive = document.querySelector(".pActive");
         // checkerMove.classList.add("pActive");
@@ -14,23 +17,38 @@ document.addEventListener('mousedown', (event) => {
         // if(currentChecker){
             
         // }
+        console.log(isDragging);
 });
+// document.addEventListener('mousedown', ()=>{
+//     console.log(isDragging);
+// })
 
-
-console.log(checkerMove.className);
-if (checkerMove.className.includes("pActive")){
-    console.log("asd");
 document.addEventListener("mousemove", e=>{
-    console.log("asd");
+    if(isDragging){
+    const pActive = document.querySelector(".pActive");
     pActive.style.top = `${e.clientY-35}px`;
     pActive.style.left = `${e.clientX-35}px`;
     // document.addEventListener("mousedown", ()=>{
     // checkerMove.classList.remove("pActive");
     // document.elementFromPoint(event.clientX, event.clientY).classList.remove("active");
     // })
-
+    }
 });
-}
+   pActive.style.top = "0px";
+    pActive.style.left = "0px";
+
+document.addEventListener('mouseup', e=>{
+    // isDragging=false;
+    // pActive.style.top = "0px";
+    // pActive.style.left = "0px";
+    console.log("w");
+    const elementUnderMouse = document.elementFromPoint(e.clientX, e.clientY);
+    checkerMove.classList.remove("pActive");
+    console.log(elementUnderMouse);
+    const currentElement = `${elementUnderMouse.className.split(" ", 1)}>.checker`;
+    console.log(currentElement);
+    document.querySelector(`${elementUnderMouse.className}>.checker`).classList.add("active");
+})
 
 // document.addEventListener('mousedown', (event)=>{
 //     if(checkerMove.classList.includes("pActive")){
