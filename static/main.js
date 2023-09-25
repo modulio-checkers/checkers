d8c837fc92679ef0310908be23eeba043256146b
81GB3N <jonas.balukonis@gmail.com>
2023-09-25 19:59:31 +0300

    biski dar maziau backroomsu

--- static/main.js
+++ static/main.js
@@ -18,6 +18,8 @@ document.addEventListener('mousedown', (e) => {
         document.querySelector(".container").innerHTML += `<div id="p1Checker"></div>`;
         const p1Checker = document.querySelector("#p1Checker");
         document.querySelector("#p1Checker").style.background = "red";
+        document.querySelector("#p1Checker").style.border = "3px solid black";
+
 
         p1Checker.style.top = `${e.clientY - 35}px`;
         p1Checker.style.left = `${e.clientX - 35}px`;
@@ -38,25 +40,34 @@ document.addEventListener("mousemove", e => {
 document.addEventListener('mouseup', e => {
     isDragging = false;
     document.querySelector("#p1Checker").remove();
-    let elementUnderMouse = document.elementFromPoint(e.clientX, e.clientY);
-    // console.log(elementUnderMouse.children[0]);
-    // console.log(elementUnderMouse.children[0].dataset.checker);
-    console.log(elementUnderMouse.classList.value.includes(invalid[0]));
-    if(elementUnderMouse.classList.value.includes(invalid[0]) || elementUnderMouse.classList.value.includes(invalid[1]) || elementUnderMouse.classList.value.includes(invalid[2])){
-        // console.log(currentChecker.classList);
-        // currentChecker.classList.add("active");
-        // console.log(currentChecker.classList.add("active"), document.querySelector(".checker28").classList.add("active"));
-        // document.querySelector(".checker28").classList.add("active");
-        // console.log(currentChecker.classList[1]);
+    let elementUnderMouse = '';
+    // if(e.clientX <= document.querySelector('html').offsetWidth && e.clientX >= 0 && 
+    // e.clientY <= document.querySelector('html').offsetHeight && e.clientY >= 0){
+    //     elementUnderMouse = document.elementFromPoint(e.clientX, e.clientY);}
+    // else{
+    //     elementUnderMouse = 0;
+    // }
+    elementUnderMouse = document.elementFromPoint(e.clientX, e.clientY);
+
+    // e.clientX > document.querySelector('html').offsetWidth || e.clientX < 0 ||
+    //  e.clientY > document.querySelector('html').offsetHeight || e.clientY < 0
+    // console.log(elementUnderMouse===null);
+    let includes;
+    if(elementUnderMouse!==null){
+        includes = elementUnderMouse.classList.value;
+    }
+    // console.log(e.clientX > document.querySelector('html').offsetWidth || e.clientX < 0);
+    // console.log(typeof elementUnderMouse === null);
+    if(elementUnderMouse === null){
         document.querySelector(`.${currentChecker.classList[1]}`).classList.add("active"); ///important, do not change
     }
-    else{
+    else if(includes.includes(invalid[0]) || includes.includes(invalid[1])
+     || includes.includes(invalid[2])){
+        document.querySelector(`.${currentChecker.classList[1]}`).classList.add("active"); ///important, do not change
+    }
+    else if(elementUnderMouse.classList.value.includes(valid)){
         elementUnderMouse.children[0].classList.add("active");
     }
-    console.log(currentChecker);
 
 })
 
-currentChecker.classList.add("active");
-
-console.log(currentChecker);
