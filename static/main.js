bd7b8c9113c2acb1a8c001d415ff9bba721aff96
81GB3N <jonas.balukonis@gmail.com>
2023-09-25 16:59:44 +0300

    istrinti js komentarai

--- static/main.js
+++ static/main.js
@@ -1,6 +1,4 @@
-// const checkerMove = document.querySelector(".player");
 let isDragging = false;
-// const pActive = document.querySelector(".pActive");
 let currentChecker;
 
 
@@ -19,16 +17,7 @@ document.addEventListener('mousedown', (e) => {
         p1Checker.style.top = `${e.clientY - 35}px`;
         p1Checker.style.left = `${e.clientX - 35}px`;
     }
-    // const pActive = document.querySelector(".pActive");
-    // checkerMove.classList.add("pActive");
-    // if(currentChecker){
-
-    // }
-    // console.log(isDragging);
 });
-// document.addEventListener('mousedown', ()=>{
-//     console.log(isDragging);
-// })
 
 document.addEventListener("mousemove", e => {
     if (isDragging) {
@@ -36,10 +25,6 @@ document.addEventListener("mousemove", e => {
         document.querySelector("#p1Checker").style.background = "red";
         p1Checker.style.top = `${e.clientY - 35}px`;
         p1Checker.style.left = `${e.clientX - 35}px`;
-        // document.addEventListener("mousedown", ()=>{
-        // checkerMove.classList.remove("pActive");
-        // document.elementFromPoint(event.clientX, event.clientY).classList.remove("active");
-        // })
     }
 });
 
@@ -47,35 +32,7 @@ document.addEventListener("mousemove", e => {
 document.addEventListener('mouseup', e => {
     isDragging = false;
     document.querySelector("#p1Checker").remove();
-    // console.log(checkerMove.classList);
-    // console.log("w");
     let elementUnderMouse = document.elementFromPoint(e.clientX, e.clientY);
-    // console.log(elementUnderMouse.className.includes("checker "));
     console.log(elementUnderMouse.children[0]);
     elementUnderMouse.children[0].classList.add("active");
-    // while(!elementUnderMouse.tagName !== 'HTML'){
-    //     elementUnderMouse.style.pointerEvent = "none";
-    //     elementUnderMouse = document.elementFromPoint(e.clientX, e.clientY);
-    // }
-    // elementUnderMouse.forEach(element => {
-    //     console.log(elementUnderMouse);
-
-    // });
-    // const currentElement = `${elementUnderMouse.className.split(" ", 1)}>.checker`;
-    // console.log(currentElement);
-    // document.querySelector(`${elementUnderMouse.className}>.checker`).classList.add("active");
 })
-
-// document.addEventListener('mousedown', (event)=>{
-//     if(checkerMove.classList.includes("pActive")){
-//         checkerMove.classList.remove("pActive");
-//         document.elementFromPoint(event.clientX, event.clientY).classList.remove("active");
-//     }
-// });
-// console.log("remove");
-// checkerMove.classList.remove("pActive");
-
-
-// document.addEventListener('mouseup', (event) => {
-//     checkerMove.classList.remove("pActive");
-// });
