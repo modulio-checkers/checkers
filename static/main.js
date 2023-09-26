c5924eebf12b55cc745324650291334f18583e42
81GB3N <jonas.balukonis@gmail.com>
2023-09-26 11:12:13 +0300

    backroom fixes

--- static/main.js
+++ static/main.js
@@ -1,8 +1,9 @@
+//Kol kas tik vienas zaidejas veliau reiks pridet padigubintas funkcijas antram
 let isDragging = false;
 let currentChecker = '';
 let playerTurn = 1;
-const invalid = ["white", "container", "active"];
-const valid = "black";
+const invalid = ["white", "container", "active", "player1-info", "player2-info", "timer1", "player1", "timer2", "player2"];
+const valid = ["black"];
 
 console.log(invalid, valid);
 
@@ -13,14 +14,11 @@ document.addEventListener('mousedown', (e) => {
     console.log(currentChecker);
     if (elementUnderMouse.className.includes("active")) {
         elementUnderMouse.classList.remove("active");
-
         isDragging = true;
         document.querySelector(".container").innerHTML += `<div id="p1Checker"></div>`;
         const p1Checker = document.querySelector("#p1Checker");
         document.querySelector("#p1Checker").style.background = "red";
         document.querySelector("#p1Checker").style.border = "3px solid black";
-
-
         p1Checker.style.top = `${e.clientY - 35}px`;
         p1Checker.style.left = `${e.clientX - 35}px`;
     }
@@ -43,14 +41,11 @@ document.addEventListener('mouseup', e => {
     let elementUnderMouse = '';
     elementUnderMouse = document.elementFromPoint(e.clientX, e.clientY);
     let includes;
-    if(elementUnderMouse!==null){
+    if(elementUnderMouse !== null){
         includes = elementUnderMouse.classList.value;
     }
-    if(elementUnderMouse === null){
-        document.querySelector(`.${currentChecker.classList[1]}`).classList.add("active"); ///important, do not change
-    }
-    else if(includes.includes(invalid[0]) || includes.includes(invalid[1])
-     || includes.includes(invalid[2])){
+    console.log(typeof includes)
+    if(elementUnderMouse === null || invalid.some(element => includes.includes(element))){
         document.querySelector(`.${currentChecker.classList[1]}`).classList.add("active"); ///important, do not change
     }
     else if(elementUnderMouse.classList.value.includes(valid)){
