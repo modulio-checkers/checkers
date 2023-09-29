0d5a83782c42e59fdeefa6e5bd3149033abac2c4
Nojus Adomaitis <nojusgaming1113@gmail.com>
2023-09-29 11:02:40 +0300

    Main communication logic & tests of move validation algorithm. (#17)
    * Main communication logic & tests of move validation algorithm.
    
    * add metadata
    
    * metadata update callback
    
    ---------
    
    Co-authored-by: Nojus <me@xe1h.xyz>

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import flask
 from flask_sqlalchemy import SQLAlchemy
+import flask_socketio as io
 from sqlalchemy.orm import DeclarativeBase
 from board import *
 from utils import *
@@ -23,6 +24,7 @@ class BoardModel(db.Model):
 
 app = flask.Flask(__name__)
 app.secret_key = 'SECRETKEYDONOTUSEINPRODUCTION'
+socketio = io.SocketIO(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db.init_app(app)
@@ -65,7 +67,7 @@ def new():
 
 
 @app.route('/game/<string:board>')
-def game(board):
+def game(board: str):
     q = db.session.query(BoardModel).filter_by(id=board).first()
     print(q)
     board = board_model_to_board(q)
@@ -74,10 +76,63 @@ def game(board):
     if board:
         if not board.is_full() and not board.is_player(flask.session['id']):
             board.set_last_player(flask.session['id'])
-            # todo
+
+            update_metadata(board)
+            # Update board player ids
+            db.session.query(BoardModel).filter_by(id=board.id).update(
+                {'player1': board.player1, 'player2': board.player2})
+            db.session.commit()
+
         return flask.render_template('index.html', board=board)
 
     return "Error"
+
+
+@socketio.on('ready')
+def ready(data: dict):
+    game_id = data.get('gameId')
+    player_id = flask.session.get('id')
+    if game_id and player_id:
+        q = db.session.query(BoardModel).filter_by(id=game_id).first()
+        board = board_model_to_board(q)
+        if board:
+            if board.is_player(player_id):
+                # OK
+                io.join_room(game_id)
+                if board.is_full():
+                    # Join room
+                    socketio.emit('ready_server')
+                    update_board(board)
+            else:
+                socketio.emit('error', {'message': 'You are not a player in this game.'})
+
+
+@socketio.on('move')
+def move(data: dict):
+    game_id = data.get('gameId')
+    from_square = data.get('from')
+    to_square = data.get('to')
+    if game_id and from_square and to_square:
+        q = db.session.query(BoardModel).filter_by(id=game_id).first()
+        board = board_model_to_board(q)
+        if board:
+            if board.is_player(flask.session['id']):
+                # OK
+                board.move(flask.session['id'], from_square, to_square)
+                db.session.query(BoardModel).filter_by(id=game_id).update(
+                    {'board': json.dumps(board.board), 'turn': board.turn})
+                db.session.commit()
+                update_board(board)
+            else:
+                socketio.emit('error', {'message': 'You are not a player in this game.'})
+
+
+def update_board(board: Board):
+    socketio.emit('update_board', {"turn": board.turn, "board": board.board}, room=board.id)
+
+
+def update_metadata(board: Board):
+    socketio.emit('update_metadata', {"player1": board.player1, "player2": board.player2}, room=board.id)
 
 
 if __name__ == '__main__':
---
+++ test_board.py
@@ -0,0 +1,70 @@
+import unittest
+from board import Board, default_board_setup
+
+
+class TestBoard(unittest.TestCase):
+
+    def setUp(self):
+        self.board = Board()
+        self.board.configure(
+            id="testID",
+            board=[0] * 64,
+            turn=1,
+            player1="player1",
+            player2="player2"
+        )
+
+    def test_incorrect_move(self):
+        self.board.turn = 1
+        self.assertEqual(self.board.move("player1", 0, 9), False,
+                         "Should return False if move is invalid")
+
+    def test_correct_move(self):
+        self.board.turn = 1
+        self.board.board[9] = 1
+        self.assertEqual(self.board.move("player1", 9, 18), True,
+                         "Should return True if move is valid")
+
+    def test_incorrect_capture(self):
+        self.board.turn = 1
+        self.assertEqual(self.board.move("player1", 8, 26), False,
+                         "Should return False if capture isn't valid")
+
+    def test_correct_capture(self):
+        self.board.board[9] = 1
+        self.board.board[18] = -1
+        self.board.turn = 1
+        self.assertEqual(self.board.move("player1", 9, 27), True,
+                         "Should return True if capture is valid")
+        self.assertEqual(self.board.board[18], 0, "Should be 0 after capture")
+
+    def test_incorrect_queen_movement(self):
+        self.board.turn = 1
+        self.board.board[56] = 2
+        self.assertEqual(self.board.move("player1", 56, 0), False,
+                         "Should return False if queen movement is invalid")
+
+    def test_pawn_promotion(self):
+        self.board.turn = 1
+        self.board.board[54] = 1
+        self.board.move("player1", 54, 63)
+        self.assertEqual(self.board.board[63], 2,
+                         "Should be 2 (queen) after pawn reaches other end.")
+
+    def test_player_2(self):
+        self.board.turn = 2
+        self.board.board[18] = -1
+        self.assertEqual(self.board.move("player2", 18, 9), True,
+                         "Should return True if move is valid")
+
+    def test_multi_capture(self):
+        self.board.turn = 1
+        self.board.board[9] = 1
+        self.board.board[18] = -1
+        self.board.board[34] = -1
+        self.assertEqual(self.board.move("player1", 9, 41), True,
+                         "Should return True if capture is valid")
+
+
+if __name__ == '__main__':
+    unittest.main()
