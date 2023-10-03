d5cf780802bee611ab5259fd18b90b461ca02931
Jonas Balukonis <139865810+81GB3N@users.noreply.github.com>
2023-10-03 12:51:08 +0300

    Movement (#22)
    * backroom fixes
    
    * dar bug fixes
    
    * kazkas
    
    * working on available squares
    
    * bozo oranziniai langeliai
    
    * available numbers function
    
    * veikia valid moves i visas puses
    
    * veikiantis dvieju zaideju
    
    * pora funkciju perkeltu
    
    * sutvarkytas abieju zaideju registravimas
    
    * changing colors bug fixed
    
    * commens deleted in index
    
    * pries pull
    
    * work in progess
    
    * kazkas
    
    * kazkas
    
    * idunno
    
    * belenkas

--- static/main.js
+++ static/main.js
@@ -27,7 +27,6 @@ const allChecker = document.querySelectorAll(".checker")
 // for(x in allChecker){
 //     console.log(x, allChecker[x].dataset.row, allChecker[x].dataset.column);
 // }
-
 function availableNumbers(checkerNum){
     if(checkerNum == 1){
         available = [document.querySelector(".square10")];
@@ -60,7 +59,6 @@ function availableNumbers(checkerNum){
     }
 }
 //pakeist, kad veiktu su grid system
-
 function oneMouseDown(elementUnderMouse, e){
     thisPlayer = "one";
     console.log(thisPlayer);
@@ -77,7 +75,6 @@ function oneMouseDown(elementUnderMouse, e){
     p1Checker.style.top = `${e.clientY - 35}px`;
     p1Checker.style.left = `${e.clientX - 35}px`;
 }
-
 function twoMouseDown(elementUnderMouse,e){
     thisPlayer = "two";
     const checkerNum = Number(currentChecker.dataset.checker);
@@ -93,13 +90,11 @@ function twoMouseDown(elementUnderMouse,e){
     p2Checker.style.top = `${e.clientY - 35}px`;
     p2Checker.style.left = `${e.clientX - 35}px`;
 }
-
 function oneMouseMove(e){
     const p1Checker = document.querySelector("#p1Checker");
     p1Checker.style.top = `${e.clientY - 35}px`;
     p1Checker.style.left = `${e.clientX - 35}px`;
 }
-
 function twoMouseMove(e){
     const p2Checker = document.querySelector("#p2Checker");
     p2Checker.style.top = `${e.clientY - 35}px`;
@@ -121,7 +116,6 @@ document.addEventListener('mousedown', e => {
         }
     }
 });
-
 document.addEventListener("mousemove", e => {
     if (isDragging) {
         if(turn % 2 == 0 && thisPlayer == "one"){
@@ -132,7 +126,6 @@ document.addEventListener("mousemove", e => {
             }
     }
 });
-
 document.addEventListener('mouseup', e => {
     console.log(available);
     isDragging = false;
@@ -181,16 +174,15 @@ document.addEventListener('mouseup', e => {
         }
     }
     console.log(available);
-
     // else if(elementUnderMouse === null || invalid.some(element => includes.includes(element))){
     //     document.querySelector(`.${currentChecker.classList[1]}`).classList.add("active"); ///important, do not change
     // }
     //senas geras kodas ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
     // document.querySelector(`.${available[x].classList[available.length-1]}`).classList.remove("orange");
     //kinda veikiantis kodas^^^^
 
 })
+
 }
 //aptvarkyti mouseup funkcija
 //move validation and update board
